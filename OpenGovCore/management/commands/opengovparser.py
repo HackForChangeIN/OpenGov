135078434a7417d6ba2a19dc4cc17c65a8a6d681
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2020-09-23 14:47:22 +0530

    Parser modified

--- OpenGovCore/management/commands/opengovparser.py
+++ OpenGovCore/management/commands/opengovparser.py
@@ -1,13 +1,18 @@
+from OpenGovCore.models import Candidate
+
+
+
 class OpenGovParser:
     url = ''
 
     def __init__(self, url):
         self.url = url
 
-    def load_candidate_data(self):
+    def load_candidate_data(self,*args):
         pass
+        
 
-    def load_questions(self):
+     def load_questions(self):
         pass
 
     def load_bills(self):
