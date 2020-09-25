f53294a07611dbe08ff342ea29416f494613f1af
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2020-09-25 16:35:00 +0530

    Loksabha and Opengov parser modified

--- OpenGovCore/management/commands/loksabhaparser.py
+++ OpenGovCore/management/commands/loksabhaparser.py
@@ -2,13 +2,14 @@ from .opengovparser import OpenGovParser
 import requests
 import shutil
 
+
 class LoksabhaParser(OpenGovParser):
 
 	def load_questions(self):
 		pass
 
 	def find_all_urls(self):
-		all_class=self.soup.find("div",attrs = {'id':'divPrint'}).find_all('a')
+		all_class = self.soup.find("div", attrs={'id': 'divPrint'}).find_all('a')
 		urls = []
 		for a_tag in all_class:
 			url = a_tag['href']
@@ -18,7 +19,7 @@ class LoksabhaParser(OpenGovParser):
 		unique_urls = list(dict.fromkeys(urls))
 		return unique_urls
 
-	def download_image(self,img_src):
+	def download_image(self, img_src):
 		response = requests.get(img_src, stream=True)
 		filename = img_src.rsplit("/")[-1]
 		file = open("media/{}".format(filename), 'wb')
@@ -34,17 +35,18 @@ class LoksabhaParser(OpenGovParser):
 		for url in urls:
 			self.url = url
 			super().load_parser()
-			img = self.soup.find("img", attrs={"id":"ContentPlaceHolder1_Image1"})
-			img_src=img['src']
+			img = self.soup.find("img", attrs={"id": "ContentPlaceHolder1_Image1"})
+			img_src = img['src']
 			image_name = self.download_image(img_src)
-
-			all_detail = self.soup.find("table",attrs = {'id':'ContentPlaceHolder1_Datagrid1'})
+			all_detail = self.soup.find(
+				"table", attrs={'id': 'ContentPlaceHolder1_Datagrid1'})
 			table_data = all_detail.find('td')
-			mp_name = table_data.tr.find('td',attrs = {'class':'gridheader1'}).text.strip()
-			items = all_detail.find_all('td',attrs = {'class':'griditem2'})
+			mp_name = table_data.tr.find(
+				'td', attrs={'class': 'gridheader1'}).text.strip()
+			items = all_detail.find_all('td', attrs={'class': 'griditem2'})
 
 			constituency = items[0].text.strip().split(" ")[0]
-			state = items[0].text.strip().rsplit("(")[-1].replace(")","")
+			state = items[0].text.strip().rsplit("(")[-1].replace(")", "")
 
 			party = items[1].text.strip()
 			if '(' in party:
@@ -52,41 +54,40 @@ class LoksabhaParser(OpenGovParser):
 			else:
 				party = party
 			email = items[2].text.strip()
-			email = email.replace('AT','@').replace('DOT','.').replace('[','').replace(']','')
-			
-			More_detail = self.soup.find("table",attrs = {'id':'ContentPlaceHolder1_DataGrid2'})
-			more_items = More_detail.find_all('td',attrs = {'class':'griditem2'})
-			
+			email = email.replace('AT', '@').replace('DOT','.').replace('[', '').replace(']', '')
+			More_detail = self.soup.find(
+				"table", attrs={'id': 'ContentPlaceHolder1_DataGrid2'})
+			more_items = More_detail.find_all('td', attrs={'class': 'griditem2'})
 			dob = more_items[2].text.strip()
 			education = more_items[9].text.strip()
-
 			profession = more_items[10].text.strip()
-			profession = profession.replace('\r','').replace('\n','')
+			profession = profession.replace('\r', '').replace('\n', '')
 			profession = " ".join(profession.split())
 
 			try:
-				permanent_address = more_items[12].text.strip() + more_items[13].text.strip()
-			
+				permanent_address = more_items[12].text.strip(
+				) + more_items[13].text.strip()
+
 			except:
-				permanent_address='Not Available '
+				permanent_address = 'Not Available '
 
 			try:
-				present_address=more_items[19].text.strip() + more_items[20].text.strip()
+				present_address = more_items[19].text.strip() + more_items[20].text.strip()
 			except:
-				present_address='Not Available'
+				present_address = 'Not Available'
 
 			try:
-				mobile=more_items[21].text.strip()
+				mobile = more_items[21].text.strip()
 			except:
-				mobile='Not Available'
+				mobile = 'Not Available'
 
-			row+=1
-			if(row == 6):
+			row += 1
+			if(row == 10):
 				break
 
 			print("Name : ", mp_name)
-			print("Constituency : ",constituency)
-			print("State : ",state)
+			print("Constituency : ", constituency)
+			print("State : ", state)
 			print("Party : ", party)
 			print("Email : ", email)
 			print("DOB : ", dob)
@@ -103,13 +104,13 @@ class LoksabhaParser(OpenGovParser):
 			data.append(email)
 			data.append(dob)
 			data.append(education)
-			data.append( profession)
+			data.append(profession)
 			data.append(permanent_address)
 			data.append(present_address)
 			data.append(mobile)
 			data.append(image_name)
-			OpenGovParser.load_candidate_data(self,*data)
-    	
+			OpenGovParser.load_candidate_data(self, *data)
+
 
 #loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
 #loksabha_parser.load_candidate_data()
--- OpenGovCore/management/commands/opengovparser.py
+++ OpenGovCore/management/commands/opengovparser.py
@@ -17,44 +17,40 @@ class OpenGovParser:
         return self.soup
 
     def load_candidate_data(self, *args):
-        mp_name = args[0]
-        constituency = args[1]
-        state = args[2]
-        party = args[3]
-        email = args[4]
-        dob = args[5]
-        education = args[6]
-        profession = args[7]
-        permanent_address = args[8]
-        present_address = args[9]
-        mobile = args[10]
-        image_name = args[11]
+        mp_name,constituency,state,party,email,dob,education,profession,permanent_address,present_address,mobile,image_name= args
+        try:
+            candidate_obj = Candidate.objects.get(name__contains = mp_name)
+            candidate_obj.name=mp_name
+            candidate_obj.dob=dob
+            candidate_obj.qualification=education
+            candidate_obj.contact_number=mobile
+            candidate_obj.email=email 
+            candidate_obj.profession=profession 
+            candidate_obj.present_address=present_address 
+            candidate_obj.permanent_address=permanent_address
+            candidate_obj.photo = image_name 
+            candidate_obj.save()      
+        except Candidate.DoesNotExist:
+            candidate_obj = Candidate.objects.create(name=mp_name, dob=dob, qualification=education,
+            contact_number=mobile, email=email, profession=profession, present_address=present_address, permanent_address=permanent_address,photo = image_name )
+    
+    def load_candidature_data(self,*args):
+        mp_name,constituency,state,party,email,dob,education,profession,permanent_address,present_address,mobile,image_name= args
         state_obj = States.objects.get(name=state)
-        constituency_obj = Parliamentary_Constituencies.objects.get(
-            name=constituency)
+        constituency_obj = Parliamentary_Constituencies.objects.get(name=constituency)
         try:
             party_obj = Parties.objects.get(party_name=party)
         except Parties.DoesNotExist:
             party_obj = Parties.objects.create(party_name=party)
-        candidate_obj = Candidate.objects.update_or_create(name=mp_name, dob=dob, qualification=education,
-        contact_number=mobile, email=email, profession=profession, present_address=present_address, permanent_address=permanent_address,photo = image_name )
-        
+        candidate_obj = Candidate.objects.get(name=mp_name, dob=dob)
+        term = Term.objects.get(term_name = "17th")
+        central_legislature = Central_Legislatures.objects.get(name = "Loksabha")
         try:
-            candidate_obj = Candidate.objects.get(name=mp_name, dob=dob)
-            term = Term.objects.get(term_name = "17th")
-            central_legislature = Central_Legislatures.objects.get(name = "Loksabha")
             candidature_obj = Candidature.objects.update_or_create(candidate_id = candidate_obj,party_id = party_obj
-            ,state_id = state_obj ,type = "MP",parliamentary_constituency_id = constituency_obj  ,term_id = term ,central_legislature_id = central_legislature,
-            )
+            ,state_id = state_obj ,type = "MP",parliamentary_constituency_id = constituency_obj  ,term_id = term ,central_legislature_id = central_legislature)
         except Candidate.DoesNotExist:
             print("Candidate data not found")
-        
-
-
-
-
-
-
+    
     def load_questions(self):
         pass
 
