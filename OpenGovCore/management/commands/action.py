da5deb49f7e8574bddc16243fb4f614f989bf76d
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2020-10-02 17:05:24 +0530

    parser modified

--- OpenGovCore/management/commands/action.py
+++ OpenGovCore/management/commands/action.py
@@ -4,12 +4,12 @@ from .question_debate import ScrapeLokSabha
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        #loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
-        #loksabha_parser.load_candidate_data()
-        #loksabha_parser.load_candidature_data()
+        loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
+        loksabha_parser.load_candidate_data()
+        loksabha_parser.load_candidature_data()
         #url = "http://loksabhaph.nic.in/Questions/Qtextsearch.aspx"
         #sc = ScrapeLokSabha(url)
         #sc.load_questions()
-        url = "http://loksabhaph.nic.in/Debates/Debatetextsearch16.aspx"
-        sc = ScrapeLokSabha(url)
-        sc.load_debates()
+        #url = "http://loksabhaph.nic.in/Debates/Debatetextsearch16.aspx"
+        #sc = ScrapeLokSabha(url)
+        #sc.load_debates()
