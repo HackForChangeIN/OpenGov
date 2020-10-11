4737bcb09fed7a28260e1879359a9315123ae5aa
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2020-10-11 12:52:09 +0530

    Changes in action file

--- OpenGovCore/management/commands/action.py
+++ OpenGovCore/management/commands/action.py
@@ -4,22 +4,22 @@ from .question_debate import ScrapeLokSabha
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        #loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
-        #loksabha_parser.load_candidate_data()
+        loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
+        loksabha_parser.load_candidate_data()
         
-        #url = "http://loksabhaph.nic.in/Questions/Qtextsearch.aspx"
-        #sc = ScrapeLokSabha(url)
-        #sc.load_questions()
-        #url = "http://loksabhaph.nic.in/Debates/Debatetextsearch16.aspx"
-        #sc = ScrapeLokSabha(url)
-        #sc.load_debates()
-        #url = "http://loksabhaph.nic.in/Legislation/NewAdvsearch.aspx"
-        #sc = ScrapeLokSabha(url)
-        #sc.load_bills()
-        #url = "http://loksabhaph.nic.in/Members/memberwisetotal.aspx"
-        #sc = ScrapeLokSabha(url)
-        #sc.load_attendance()
-        count = 0
+        url = "http://loksabhaph.nic.in/Questions/Qtextsearch.aspx"
+        sc = ScrapeLokSabha(url)
+        sc.load_questions()
+        url = "http://loksabhaph.nic.in/Debates/Debatetextsearch16.aspx"
+        sc = ScrapeLokSabha(url)
+        sc.load_debates()
+        url = "http://loksabhaph.nic.in/Legislation/NewAdvsearch.aspx"
+        sc = ScrapeLokSabha(url)
+        sc.load_bills()
+        url = "http://loksabhaph.nic.in/Members/memberwisetotal.aspx"
+        sc = ScrapeLokSabha(url)
+        sc.load_attendance()
+        """count = 0
         while True:
             try:
                 loksabha_parser = LoksabhaParser(url = "http://loksabhaph.nic.in/Members/AlphabeticalList.aspx")
@@ -66,4 +66,4 @@ class Command(BaseCommand):
                 if count == 4:
                     break
                 else:
-                    continue
+                    continue"""
