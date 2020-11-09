6b9cceed81cc20b25ea2e596ee98e7f889459c27
devvsaurabh <rajsaurabh84@gmail.com>
2020-11-09 11:44:02 +0530

    olddata.py file modified

--- OpenGovCore/management/commands/olddata.py
+++ OpenGovCore/management/commands/olddata.py
@@ -7,10 +7,10 @@ from .oldloksabhaparser import OldLoksabhaParser
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         #16 th term Loksabha
-        #url = "http://loksabhaph.nic.in/Members/lokaralpha.aspx?lsno=16&tab=0"
+        url = "http://loksabhaph.nic.in/Members/lokaralpha.aspx?lsno=16&tab=0"
+        lok = OldLoksabhaParser(url="./OpenGovCore/data/raw_data/old_loksabha_data.xlsx", term = "16")
+        lok.load_candidate_data()
+        #15 th
+        #url = "http://loksabhaph.nic.in/Members/lokaralpha.aspx?lsno=15&tab=1"
         #lok = OldLoksabhaParser(url=url)
         #lok.load_data()
-        #15 th
-        url = "http://loksabhaph.nic.in/Members/lokaralpha.aspx?lsno=15&tab=1"
-        lok = OldLoksabhaParser(url=url)
-        lok.load_data()
